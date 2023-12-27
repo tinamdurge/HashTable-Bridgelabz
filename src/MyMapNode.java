05755aa32e8e1e8af7f09298fe4ce2dcf76e6384
Tinam Durge <tinamdurge02@gmail.com>
2023-12-27 12:09:44 +0530

    Used hashcode for hashmap operations

--- src/MyMapNode.java
+++ src/MyMapNode.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class MyMapNode {
+public class MyMapNode<K extends Comparable<K>, V> {
+    private K key;
+    private V value;
+    MyMapNode<K, V> next;
+
+    public MyMapNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
 }
